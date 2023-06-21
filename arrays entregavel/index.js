294a54dea8ffcd2cd6899fd68aa6ded528fd71eb
Yangmape <134799815+Mateuspsantos1@users.noreply.github.com>
2023-06-21 20:39:53 -0300

    Add files via upload

---
+++ arrays entregavel/index.js
@@ -0,0 +1,8 @@
+const nome = prompt("qual seu nome?")
+const nomePai = prompt(" qual nome do seu pai?")
+const nomeMae = prompt(" qual nome da sua mãe")
+
+const familia = nome.concat(" " + nomePai + " " + nomeMae)
+
+
+alert("sua familia é composta por: " + familia)
